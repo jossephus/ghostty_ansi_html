d181ea807a853143a60a93f3cb4f5eba7245a8f2
jossephus <yosephtuemay64@gmail.com>
2025-11-01 21:43:24 +0300

    feat: add napigen for nodejs compat

--- lib/build.zig
+++ lib/build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const napigen = @import("napigen");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -27,4 +28,25 @@ pub fn build(b: *std.Build) !void {
     });
 
     b.installArtifact(root_lib);
+
+    const node_mod = b.createModule(.{
+        .root_source_file = b.path("node.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+
+    const node_lib = b.addLibrary(.{
+        .name = "example",
+        .root_module = node_mod,
+    });
+
+    // Add napigen
+    napigen.setup(node_lib);
+
+    // Build the lib
+    b.installArtifact(node_lib);
+
+    // Copy the result to a *.node file so we can require() it
+    const copy_node_step = b.addInstallLibFile(node_lib.getEmittedBin(), "example.node");
+    b.getInstallStep().dependOn(&copy_node_step.step);
 }
---
+++ lib/napigen/example/build.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const napigen = @import("napigen");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addSharedLibrary(.{
+        .name = "example",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Add napigen
+    napigen.setup(lib);
+
+    // Build the lib
+    b.installArtifact(lib);
+
+    // Copy the result to a *.node file so we can require() it
+    const copy_node_step = b.addInstallLibFile(lib.getEmittedBin(), "example.node");
+    b.getInstallStep().dependOn(&copy_node_step.step);
+}
