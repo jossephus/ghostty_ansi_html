7d401d6bee0f40a6a9cd5137d288e7a161f4f9bd
Josseph <46337696+jossephus@users.noreply.github.com>
2025-11-05 09:36:40 +0300

    feat: setup ci workflows for release

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    zig,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [
+          (final: prev: {
+            zigpkgs = zig.packages.${system};
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
+        packages = with pkgs; [
+          zigpkgs."0.15.2"
+          bun
+          nodejs_24
+        ];
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+          nativeBuildInputs = with pkgs; [];
+        };
+      }
+    );
+}
+
---
+++ scripts/release.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+tag=$1
+
+if [ -z "$tag" ]; then
+  echo "Tag should be specified to make a release"
+  exit 1
+fi
+
+cd packages/core/lib
+zig build
+cd ../../../
+
+platform_directories=(
+  "./packages/core/lib/zig-out/lib/libghostty-ansi-html.so"
+  "./packages/core/lib/zig-out/lib/libghostty-ansi-html-linux.node"
+  "./packages/core/lib/zig-out/bin/ghostty-ansi-html.dll"
+  "./packages/core/lib/zig-out/lib/libghostty-ansi-html-windows.node"
+)
+
+package_dirs=(
+  "packages/platforms/linux-x64"
+  "packages/platforms/linux-x64-node"
+  "packages/platforms/win32-x64"
+  "packages/platforms/win32-x64-node"
+)
+
+for i in "${!platform_directories[@]}"; do
+  native_lib="${platform_directories[$i]}"
+  package_dir="${package_dirs[$i]}"
+  cp -rvf "$native_lib" "$package_dir"
+  sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$tag\"/" "$package_dir/package.json"
+  cd "$package_dir" && bun publish
+done
+
+cd packages/core
+bun update ghostty-ansi-html-linux-x64 
+bun update ghostty-ansi-html-linux-x64-node
+bun update ghostty-ansi-html-win32-x64
+bun update ghostty-ansi-html-linux-x64-node
+bun run build
+bun publish
